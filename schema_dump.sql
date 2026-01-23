3dbf33e6f20e15e7411b5cda034641d4631e44e1
20241711 <20241711@iade.pt>
2026-01-23 10:57:03 +0000

    Adicionada nome da BD

--- schema_dump.sql
+++ schema_dump.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `configuracoes`
 --
 
+--CREATE DATABASE IF NOT EXISTS
+CREATE DATABASE IF NOT EXISTS AquaSense;
+USE AquaSense;
+
 DROP TABLE IF EXISTS `configuracoes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
